9dc161fe0d829b13e0156f91799e30c838c64d9e
ハトネコエ <radio7954@gmail.com>
2015-08-30 21:23:28 +0900

    ADD csv-mode

--- init_.el
+++ init_.el
@@ -25,7 +25,7 @@
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
 (let ( (default-directory
-         (file-name-as-directory (concat user-emacs-directory "site-lisp")))
+         (file-name-as-directory (concat user-emacs-directory "elpa")))
        )
   (add-to-list 'load-path default-directory)
 ;;  (add-to-list 'load-path "~/.emacs.d/elpa/")
@@ -118,10 +118,10 @@
 
 (setq default-frame-alist
       (append '(
-		;; (width                . 77 ) ; フレーム幅
-		;; (height               . 44 ) ; フレーム高
-		;; (left                 . 81 ) ; 配置左位置
-		;; (top                  .  2 ) ; 配置上位置
+             ;; (width                . 85)  ; フレーム幅
+             ;; (height               . 38 ) ; フレーム高
+             ;; (left                 . 70 ) ; 配置左位置
+             ;; (top                  . 28 ) ; 配置上位置
                 (line-spacing         . 0  ) ; 文字間隔
                 (left-fringe          . 10 ) ; 左フリンジ幅
                 (right-fringe         . 11 ) ; 右フリンジ幅
@@ -130,7 +130,7 @@
                 (vertical-scroll-bars . 1  ) ; スクロールバー
                 (scroll-bar-width     . 14 ) ; スクロールバー幅
                 (cursor-type          . box) ; カーソル種別
-                (alpha                . 94) ; 透明度 
+                (alpha                . 95) ; 透明度 
                ) default-frame-alist) )
 (setq initial-frame-alist default-frame-alist)
 
@@ -155,7 +155,51 @@
 ;;; http://www.bookshelf.jp/soft/meadow_30.html#SEC419              ;;;
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
-(require 'windowsize-save)
+(defun my-window-size-save ()
+  (let* ((rlist (frame-parameters (selected-frame)))
+         (ilist initial-frame-alist)
+         (nCHeight (frame-height))
+         (nCWidth (frame-width))
+         (tMargin (if (integerp (cdr (assoc 'top rlist)))
+                      (cdr (assoc 'top rlist)) 0))
+         (lMargin (if (integerp (cdr (assoc 'left rlist)))
+                      (cdr (assoc 'left rlist)) 0))
+         buf
+         (file "~/.framesize.el"))
+    (if (get-file-buffer (expand-file-name file))
+        (setq buf (get-file-buffer (expand-file-name file)))
+      (setq buf (find-file-noselect file)))
+    (set-buffer buf)
+    (erase-buffer)
+    (insert (concat
+             ;; 初期値をいじるよりも modify-frame-parameters
+             ;; で変えるだけの方がいい?
+             "(delete 'width initial-frame-alist)\n"
+             "(delete 'height initial-frame-alist)\n"
+             "(delete 'top initial-frame-alist)\n"
+             "(delete 'left initial-frame-alist)\n"
+             "(setq initial-frame-alist (append (list\n"
+             "'(width . " (int-to-string nCWidth) ")\n"
+             "'(height . " (int-to-string nCHeight) ")\n"
+             "'(top . " (int-to-string tMargin) ")\n"
+             "'(left . " (int-to-string lMargin) "))\n"
+             "initial-frame-alist))\n"
+             ;;"(setq default-frame-alist initial-frame-alist)"
+             ))
+    (save-buffer)
+    ))
+
+(defun my-window-size-load ()
+  (let* ((file "~/.framesize.el"))
+    (if (file-exists-p file)
+        (load file))))
+
+(my-window-size-load)
+
+;; Call the function above at C-x C-c.
+(defadvice save-buffers-kill-emacs
+  (before save-frame-size activate)
+  (my-window-size-save))
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
@@ -655,6 +699,11 @@
 ;; コンパイルをF12で
 (global-set-key [f12] 'compile)
 
+;; バッファを閉じる Close Buffer
+(global-set-key (kbd "C-c b") 'kill-buffer)
+(global-set-key (kbd "M-RET") 'kill-buffer) ; Alt + Enter
+(global-set-key [f5]          'kill-buffer)
+
 ;; タブ切り替え @tabber
 (global-set-key (kbd "<C-tab>")   'tabbar-forward-tab)
 (global-set-key [f6]              'tabbar-forward-tab)
@@ -663,10 +712,8 @@
 (global-set-key (kbd "<C-S-tab>") 'tabbar-backward-tab)
 (global-set-key [f7]              'tabbar-backward-tab)
 
-;; バッファを閉じる Close Buffer
-(global-set-key (kbd "C-c b") 'kill-buffer)
-(global-set-key (kbd "M-RET") 'kill-buffer) ; Alt + Enter
-(global-set-key [f8]          'kill-buffer)
+;; F8キーで shell を開く
+(global-set-key [f8] 'shell)
 
 ;; コピー (デフォの M-w も可能なまま)
 (define-key global-map (kbd "C-S-w") 'copy-region-as-kill)
@@ -695,19 +742,9 @@
 (global-set-key (kbd "C-x C-o") 'recentf-open-files)
 (global-set-key (kbd "C-x o")   'recentf-open-files)
 
-;; コメント化
+;; Alt + ; だけでなく Ctrl + ; , Ctrl + / でもコメント化
 (global-set-key (kbd "C-;") 'comment-dwim)
 (global-set-key (kbd "C-/") 'comment-dwim)
-
-;; 横（水平）に2画面
-(global-set-key (kbd "C-x C-2") 'split-window-right)
-
-;; バッファーを読み込みなおす
-(global-set-key [f5] 'revert-buffer)
-
-;; shell を開く
-(global-set-key (kbd "M-s h") 'shell)
-
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
@@ -733,7 +770,235 @@ ac-source-abbrev
 ;;;; ac-user-dict のインポート                   ;;;;
 ;;;; http://fukuyama.co/emacs-auto-complete      ;;;;
 
-(require 'ac-user-dict-import)
+;; ユーザ辞書の場所
+(defvar ac-user-dict-dir (expand-file-name "~/.emacs.d/ac-user-dict/"))
+
+;; コンプリート時の動作 - 候補の末尾に()があればその内にカーソルを置く
+(defun ac-go-into-braces-action ()
+  (save-restriction
+    (narrow-to-region (point) (- (point) 2))
+    (if (re-search-backward "()" nil t)
+        (forward-char))))
+
+;; 直前の文字を区別して辞書を使用する - 何か1文字+ドットの後の場合に補完する
+(defun ac-js-dot-prefix ()
+  "`x.' prefix."
+  (if (re-search-backward ".\\.\\(.*\\)" nil t)
+      ;; ".\\.\\(.*\\)" 何か一文字[.] ドット[\\.] 補完開始点[\\(.*\\)]
+      (match-beginning 1)))
+
+;; メニューで選択中の候補の色設定
+(defface ac-my-selection-face
+  '((t (:background "#000080" :foreground "#ffffff")))
+  "Face for selectied candidates."
+  :group 'auto-complete)
+
+;;; 辞書1 (underscore.js)
+;; 色設定
+(defface ac-underscore-js-candidate-face
+  '((t (:background "#730CE8" :foreground "#eeeeee")))
+  "Face for underscore.js candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-underscore-js-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "underscore-js")))
+;; 辞書1の設定
+(defvar ac-source-underscore-js-dict
+  '((candidates . ac-underscore-js-cache) ;; 候補の情報源 これ以下はオプション
+    (candidate-face . ac-underscore-js-candidate-face) ;; 候補の色設定
+    (selection-face . ac-my-selection-face) ;; 選択中の色設定
+    (prefix . ac-js-dot-prefix) ;; 直前の文字の条件
+    (action . ac-go-into-braces-action) ;; 補完後の動作
+    (symbol . "underscore.js") ;; ライブラリ名 (無理矢理。本来の意図とは違うはず)
+    ;; (requires . 2) ;; 補完が開始される最低入力文字数を上書き
+    ;; (limit . 30) ;; 候補を一度に表示する上限数を上書き
+    ))
+
+;;; 辞書2 (jquery)
+;; 色設定
+(defface ac-jquery-candidate-face
+  '((t (:background "#1f679a" :foreground "#eeeeee")))
+  "Face for jquery candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-jquery-method1-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "jquery-method1")))
+;; 辞書2の設定
+(defvar ac-source-jquery-method-dict1
+  '((candidates . ac-jquery-method1-cache)
+    (candidate-face . ac-jquery-candidate-face)
+    (selection-face . ac-my-selection-face)
+    (prefix . ac-js-dot-prefix)
+    (action . ac-go-into-braces-action)
+    (symbol . "jquery method1")
+    ))
+
+;;; 辞書3 (jquery)
+;; 直前の文字の条件 (`jQuery.'または`$.')
+(defun ac-jquery-method2-prefix ()
+  "`$' or `jQuery' prefix."
+  (if (re-search-backward "\\(jQuery\\|\\$\\)\\.\\(.*\\)" nil t)
+      (match-beginning 2)))
+;; 情報源に辞書ファイルを指定
+(defvar ac-jquery-method2-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "jquery-method2")))
+;; 辞書3の設定
+(defvar ac-source-jquery-method-dict2
+  '((candidates . ac-jquery-method2-cache)
+    (candidate-face . ac-jquery-candidate-face)
+    (selection-face . ac-my-selection-face)
+    (prefix . ac-jquery-method2-prefix)
+    (action . ac-go-into-braces-action)
+    (symbol . "jquery method2")
+    ))
+
+;;; 辞書4 (jqueryセレクタ)
+;; 直前の文字の条件 (`x:')
+(defun ac-jquery-selector-prefix ()
+  "`x:' prefix."
+  (if (re-search-backward ".\\:\\(.*\\)" nil t)
+      (match-beginning 1)))
+;; 色設定
+(defface ac-jquery-selector-candidate-face
+  '((t (:background "#1B919A" :foreground "#eeeeee")))
+  "Face for jquery selector candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-jquery-selector-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "jquery-selector")))
+;; 辞書4の設定
+(defvar ac-source-jquery-selector-dict
+  '((candidates . ac-jquery-selector-cache)
+    (candidate-face . ac-jquery-selector-candidate-face)
+    (selection-face . ac-my-selection-face)
+    (prefix . ac-jquery-selector-prefix)
+    (action . ac-go-into-braces-action)
+    (symbol . "jQuery selector")
+    ))
+
+;; 使用する辞書・情報源を選択
+(defun ac-js-mode-setup ()
+  (setq ac-sources
+        '(
+          ac-source-abbrev
+          ac-source-words-in-same-mode-buffers
+          ;; ac-source-yasnippet
+          ac-source-filename
+          ;; 優先順位で並べる (prefixを指定すると排他的になる; x.に$.が含まれる)
+
+	  ;; とりあえず jQuery 今は使わないのでコメントアウト
+	  ;; ac-source-jquery-method-dict2  ; prefix `$.'
+          ;; ac-source-jquery-method-dict1  ; prefix `x.'
+          ;; ac-source-underscore-js-dict   ; prefix `x.'
+          ;; ac-source-jquery-selector-dict ; prefix `x:'
+          )))
+;; メジャーモードに反映させる
+ (add-hook 'js-mode-hook 'ac-js-mode-setup)
+ (add-hook 'js2-mode-hook 'ac-js-mode-setup)
+
+;; 辞書間で重複するものが削除されるので
+;; ~/.emacs.d/elisp/auto-complete/auto-complete.el L1052 (delete-dups candidates) をコメントアウト
+
+;; ac-disable-facesの初期値は
+;; (font-lock-comment-face font-lock-string-face font-lock-doc-face)
+;; font-lock-string-faceがあるとクオートで囲まれた部分"..."で
+;; auto-completeが反応しなくなり、セレクタを補完できないので次のように
+(setq ac-disable-faces '(font-lock-comment-face font-lock-doc-face))
+
+
+;;;;  HTML, CSS, PHP のための auto-complete ;;;;
+
+;;; 辞書1 (css-include3)
+;; 色設定
+(defface ac-css-include3-candidate-face
+  '((t (:background "#D12EA6" :foreground "#260D0A")))
+  "Face for css-include3 candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-css-include3-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "css-include3")))
+;; 辞書1の設定
+(defvar ac-source-css-include3-dict
+  '((candidates . ac-css-include3-cache) ;; 候補の情報源 これ以下はオプション
+    (candidate-face . ac-css-include3-candidate-face) ;; 候補の色設定
+    (selection-face . ac-my-selection-face) ;; 選択中の色設定
+    (action . ac-go-into-braces-action) ;; 補完後の動作
+    (symbol . "CSS") ;; ライブラリ名 (無理矢理。本来の意図とは違うはず)
+    ))
+
+;;; 辞書2 (css-webkit)
+;; 色設定
+(defface ac-css-webkit-candidate-face
+  '((t (:background "#FF5445" :foreground "#260D0A")))
+  "Face for css-webkit candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-css-webkit-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "css-webkit")))
+;; 辞書1の設定
+(defvar ac-source-css-webkit-dict
+  '((candidates . ac-css-webkit-cache) ;; 候補の情報源 これ以下はオプション
+    (candidate-face . ac-css-webkit-candidate-face) ;; 候補の色設定
+    (selection-face . ac-my-selection-face) ;; 選択中の色設定
+    (action . ac-go-into-braces-action) ;; 補完後の動作
+    (symbol . "CSS-webkit") ;; ライブラリ名 (無理矢理。本来の意図とは違うはず)
+    ))
+
+;;; 辞書3 (css-mozilla)
+;; 色設定
+(defface ac-css-mozilla-candidate-face
+  '((t (:background "#FF682C" :foreground "#260D0A")))
+  "Face for css-mozilla candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-css-mozilla-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "css-mozilla")))
+;; 辞書1の設定
+(defvar ac-source-css-mozilla-dict
+  '((candidates . ac-css-mozilla-cache) ;; 候補の情報源 これ以下はオプション
+    (candidate-face . ac-css-mozilla-candidate-face) ;; 候補の色設定
+    (selection-face . ac-my-selection-face) ;; 選択中の色設定
+    (action . ac-go-into-braces-action) ;; 補完後の動作
+    (symbol . "CSS-moz") ;; ライブラリ名 (無理矢理。本来の意図とは違うはず)
+    ))
+
+
+;;; 辞書4 (HTML)
+;; 色設定
+(defface ac-html-sakura-candidate-face
+  '((t (:background "#deaa9e" :foreground "#220")))
+  "Face for html-sakura candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-html-sakura-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "html-sakura")))
+;; 辞書1の設定
+(defvar ac-source-html-sakura-dict
+  '((candidates . ac-html-sakura-cache) ;; 候補の情報源 これ以下はオプション
+    (candidate-face . ac-html-sakura-candidate-face) ;; 候補の色設定
+    (selection-face . ac-my-selection-face) ;; 選択中の色設定
+    (action . ac-go-into-braces-action) ;; 補完後の動作
+    (symbol . "HTML") ;; ライブラリ名 (無理矢理。本来の意図とは違うはず)
+    ))
+
+
+;;; 辞書5 (PHP)
+;; 色設定
+(defface ac-php-sakura-candidate-face
+  '((t (:background "#736eb3" :foreground "#220")))
+  "Face for php-sakura candidates."
+  :group 'auto-complete)
+;; 情報源に辞書ファイルを指定
+(defvar ac-php-sakura-cache
+  (ac-file-dictionary (concat ac-user-dict-dir "php-sakura")))
+;; 辞書1の設定
+(defvar ac-source-php-sakura-dict
+  '((candidates . ac-php-sakura-cache) ;; 候補の情報源 これ以下はオプション
+    (candidate-face . ac-php-sakura-candidate-face) ;; 候補の色設定
+    (selection-face . ac-my-selection-face) ;; 選択中の色設定
+    (action . ac-go-into-braces-action) ;; 補完後の動作
+    (symbol . "PHP") ;; ライブラリ名 (無理矢理。本来の意図とは違うはず)
+    ))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -759,27 +1024,128 @@ ac-source-abbrev
 (add-to-list 'auto-mode-alist '("\\(.html?\\|.css\\|.php\\)\\'"
 				. web-mode))
 
-(require 'web-mode-my-setting)
-
 ;; js2-mode
-(add-to-list 'auto-mode-alist '("\\.js\\'"          . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'"         . js2-mode))
 
 ;; json-mode
-(add-to-list 'auto-mode-alist '("\\.json\\'"        . json-mode))
-
-;; csv-mode
-(add-to-list 'auto-mode-alist '("\\.csv\\'"         . csv-mode))
+(add-to-list 'auto-mode-alist '("\\.json\\'"         . json-mode))
 
 ;; gitignore
-(add-to-list 'auto-mode-alist '("\\.gitignore\\'"   . gitignore-mode))
+(add-to-list 'auto-mode-alist '("\\.gitignore\\'"     . gitignore-mode))
 
 ;; gitconfig
-(add-to-list 'auto-mode-alist '("\\.gitconfig\\'"   . gitconfig-mode))
+(add-to-list 'auto-mode-alist '("\\.gitconfig\\'"     . gitconfig-mode))
 
 
-;; Windows Power Shell mode
-(autoload 'powershell-mode "powershell-mode" "A editing mode for Microsoft PowerShell." t)
-(add-to-list 'auto-mode-alist '("\\.ps1\\'" . powershell-mode))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;  @web mode   ;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;:;;;;;;
+
+(setq web-mode-engines-alist
+      '(
+	("html"        . "\\.html?\\'")
+	("css"         . "\\.css\\'")
+	("javascript"  . "\\.js\\'")
+	("php"         . "\\(.phtml?\\|.php\\)\\'")
+	("blade"       . "\\.blade\\'")
+	("abbrev"      . "\\abbrev_defs\\'")
+	)
+)
+
+(add-hook 'web-mode-hook 'my-web-mode-hook)
+
+(defun my-web-mode-hook ()
+  "Hooks for Web mode."
+
+  ;; @auto-complete のための設定
+  ;;;;  html
+  ;; (require 'ac-html)
+  ;; (add-hook 'web-mode-hook 'ac-html-enable)
+  
+  (setq web-mode-ac-sources-alist
+  	'(
+  	  ("css"  . (;ac-source-words-in-buffer
+		     ac-source-abbrev
+		     ac-source-files-in-current-dir
+		     ac-source-css-include3-dict
+		     ac-source-css-webkit-dict  
+		     ac-source-css-mozilla-dict ; prefix `x.'
+		     ac-source-css-property
+		     ))
+  	  ("html" . (;ac-source-words-in-buffer
+		     ac-source-abbrev
+		     ac-source-files-in-current-dir
+		     ac-source-html-sakura-dict
+		     ))
+  	  ("php"  . (;ac-source-words-in-buffer
+		     ac-source-abbrev
+		     ac-source-files-in-current-dir
+		     ac-source-php-sakura-dict
+		     `$.'ac-source-html-sakura-dict ;; prefix `$.' これを付けることで、phpタグの外でしかhtmlの候補が出ない
+		     ))
+	  ))
+  
+  ;; <> </> で囲んでいるところを光らせる
+  (setq web-mode-enable-current-column-highlight t)
+  (setq web-mode-enable-current-element-highlight t)
+
+  (setq web-mode-enable-auto-closing   t) ; </ と入力したら 補完してくれる
+  (setq web-mode-enable-auto-opening   t)
+  (setq web-mode-enable-auto-expanding t) ; d/ と入力したら<div></div>など
+  (setq web-mode-enable-auto-indentation t) ; 閉じかっこ入力で自動的にインデント直す
+  
+  ;; CSS 色付け
+  (setq web-mode-enable-css-colorization t)
+
+  ;; Tabのサイズの設定
+  (setq tab-width 2)
+  (setq web-mode-markup-indent-offset 2)  ;; HTML indent  2x2/2= 2文字インデント
+  (setq web-mode-css-indent-offset    4)  ;; CSS  indent  2x4/2= 4文字インデント
+  (setq web-mode-code-indent-offset   4)  ;; script indent(js,php,etc..)
+  
+  ;; 変更日時の自動修正
+  ;; (setq time-stamp-line-limit -200)
+  ;; (if (not (memq 'time-stamp write-file-hooks))
+  ;;     (setq write-file-hooks
+  ;;           (cons 'time-stamp write-file-hooks)))
+  ;; (setq time-stamp-format " %3a %3b %02d %02H:%02M:%02S %:y %Z")
+  ;; (setq time-stamp-start "Last modified:")
+  ;; (setq time-stamp-end "$")
+  ;; htmlの内容をインデント
+  ;; TEXTAREA等の中身をインデントすると副作用が起こったりするので
+  ;; デフォルトではインデントしない
+  ;;(setq web-mode-indent-style 2)
+  ;; コメントのスタイル
+  ;;   1:htmlのコメントスタイル(default)
+  ;;   2:テンプレートエンジンのコメントスタイル
+  ;;      (Ex. {# django comment #},{* smarty comment *},{{-- blade comment --}})
+  (setq web-mode-comment-style 2)
+  css,js,php,etc..の範囲をbg色で表示
+  (setq web-mode-enable-block-faces t)
+  (custom-set-faces
+   '(web-mode-server-face
+     ((t (:background "grey"))))                  ; template Blockの背景色
+   '(web-mode-css-face
+     ((t (:background "grey18"))))                ; CSS Blockの背景色
+   '(web-mode-javascript-face
+     ((t (:background "grey36"))))                ; javascript Blockの背景色
+   )
+  (setq web-mode-enable-heredoc-fontification t)
+  )
+
+;; 色の設定
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-rule-face ((t (:foreground "#A0D8EF"))))
+ '(web-mode-doctype-face ((t (:foreground "#bbff22"))))
+ '(web-mode-html-attr-name-face ((t (:foreground "#ed7dd7"))))
+ '(web-mode-html-attr-value-face ((t (:foreground "light salmon"))))
+ '(web-mode-html-tag-face ((t (:foreground "light sea green" :weight bold)))))
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
@@ -804,12 +1170,12 @@ ac-source-abbrev
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
 ;; emacs-server起動
-;; (require 'server)
-;; (defun server-ensure-safe-dir (dir) "Noop" t)
-;; (setq server-socket-dir "~/.emacs.d")
-;; (unless (server-running-p)
-;;   (server-start)
-;; )
+(require 'server)
+(defun server-ensure-safe-dir (dir) "Noop" t)
+(setq server-socket-dir "~/.emacs.d")
+(unless (server-running-p)
+  (server-start)
+)
 
 
 
@@ -818,8 +1184,10 @@ ac-source-abbrev
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(column-number-mode t)
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
- )
+ '(tabbar-mode t nil (tabbar)))
 ;; Local Variables:
 ;; coding: utf-8
 ;; mode: emacs-lisp
