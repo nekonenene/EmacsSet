47f874c539bb4e5c52aa784423d1b0bb6a53b76a
ハトネコエ hatonekoe@gmail.com <radio7954@gmail.com>
2015-09-16 10:54:46 +0900

    Recall emacs-server function

--- init.el
+++ init.el
@@ -917,12 +917,12 @@
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
 ;; emacs-server起動
-;; (require 'server)
-;; (defun server-ensure-safe-dir (dir) "Noop" t)
-;; (setq server-socket-dir "~/.emacs.d")
-;; (unless (server-running-p)
-;;   (server-start)
-;; )
+(require 'server)
+(defun server-ensure-safe-dir (dir) "Noop" t)
+(setq server-socket-dir "~/.emacs.d")
+(unless (server-running-p)
+  (server-start)
+)
 
 
 (custom-set-variables
