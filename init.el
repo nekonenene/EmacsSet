1be5060ec340340ee35572baf4377e9496a3d6f1
ハトネコエ hatonekoe@gmail.com <radio7954@gmail.com>
2015-10-23 01:15:57 +0900

    comment-out lines associated bash. for Python RegExp of Emacs

--- init.el
+++ init.el
@@ -548,13 +548,13 @@
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 
 (require 'shell)
-(setq explicit-shell-file-name "bash.exe")
-(setq shell-command-switch "-c")
-(setq shell-file-name "bash.exe")
+(setq explicit-shell-file-name "bash")
+;(setq shell-command-switch "-c")
+;(setq shell-file-name "bash.exe")
 
 ;; (M-! and M-| and compile.el)
-(setq shell-file-name "bash.exe")
-(modify-coding-system-alist 'process ".*sh\\.exe" 'utf-8)
+;(setq shell-file-name "bash.exe")
+;(modify-coding-system-alist 'process ".*sh\\.exe" 'utf-8)
 
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
