0408cda54e46460131675b8686c212f8a886d577
ハトネコエ <hato.nekoe@gmail.com>
2016-08-20 22:17:50 +0900

    使っていないので migemo をばっさり削除

--- init.el
+++ init.el
@@ -314,31 +314,6 @@
 (define-key isearch-mode-map (kbd "C-g")
   '(lambda() (interactive) (isearch-done)))
 
-;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
-;;; @ search - migemo                                               ;;;
-;;;   https://github.com/emacs-jp/migemo                            ;;;
-;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
-
-;; (require 'migemo)
-
-;; (defvar migemo-command nil)
-;; (setq migemo-command "cmigemo")
-
-;; (defvar migemo-options nil)
-;; (setq migemo-options '("-q" "--emacs"))
-
-;; (defvar migemo-dictionary nil)
-;; (setq migemo-dictionary "C:/emacs/cmigemo-win32/dict/utf-8/migemo-dict")
-
-;; (defvar migemo-user-dictionary nil)
-
-;; (defvar migemo-regex-dictionary nil)
-
-;; (defvar migemo-coding-system nil)
-;; (setq migemo-coding-system 'utf-8-unix)
-
-;; (load-library "migemo")
-
 
 ;;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ;;;
 ;;; @ search - hi-lock（ハイライト）を有効に                        ;;;
